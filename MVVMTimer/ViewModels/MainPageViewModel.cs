a0d3fd4f0c3dd45d67f04c9b3451eba17c4bc387
jumpman <joseph.bollman@gmail.com>
2020-04-02 22:14:59 -0600

    init commit

---
+++ MVVMTimer/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MVVMTimer.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        public MainPageViewModel()
+        {
+            this.DateTime = DateTime.Now;
+            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            {
+                this.DateTime = DateTime.Now;
+                return true;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        DateTime dateTime;
+
+        public DateTime DateTime
+        {
+            get => dateTime;
+            set
+            {
+                if(dateTime != value)
+                {
+                    dateTime = value;
+                    if(PropertyChanged!=null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+
+                /*
+                appDateTime = value;
+                var args = new PropertyChangedEventArgs(nameof(DateTime)); //name of the method here.
+
+                PropertyChanged?.Invoke(this, args);        //? is sugar to check if its null
+                */
+            }
+
+        }
+    }
+}
