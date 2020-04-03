e56f70984c53246f0ceda2b4fced635de24715fe
jumpman <joseph.bollman@gmail.com>
2020-04-03 16:18:56 -0600

    played around with string formatters and timers. I have 1 timer that starts at 0 and loops at 60, another timer starts at 60 and loops at 0 I need to put a stop function to stop the timer.

--- MVVMTimer/ViewModels/MainPageViewModel.cs
+++ MVVMTimer/ViewModels/MainPageViewModel.cs
@@ -1,19 +1,63 @@
 ﻿using System;
+using System.Timers;
 using System.ComponentModel;
 using Xamarin.Forms;
+using System.Diagnostics;
 
 namespace MVVMTimer.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
+        int counter = 60;
+        Stopwatch stopWatch = new Stopwatch();
+        string timer;
+        private bool timeRunning;
+        Stopwatch sixtyStopWatch = new Stopwatch();
+        string sCounter;
+
         public MainPageViewModel()
         {
+            sixtyStopWatch.Start();
+            Device.StartTimer(TimeSpan.FromSeconds(0), () =>
+            {
+                sCounter = (counter - sixtyStopWatch.Elapsed.Seconds).ToString();
+                Timer = sCounter;
+                //Timer = sixtyStopWatch.Elapsed.Seconds.ToString();
+                return true;
+            });
+
+            
+           
+
+
+
+            Device.StartTimer(TimeSpan.FromSeconds(0), () =>
+            {
+                stopWatch.Start();
+                StopWatchSeconds = stopWatch.Elapsed.Seconds.ToString();
+
+                /*
+                counter--;
+                if (!(counter <= 0))
+                    return true;
+                return false;
+                */
+                return true;
+            });
+
+
+
+
+            // timer = new System.Timers.Timer(60);
             this.DateTime = DateTime.Now;
-            Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+            Device.StartTimer(TimeSpan.FromSeconds(0), () =>
             {
                 this.DateTime = DateTime.Now;
                 return true;
             });
+
+
+           
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -24,6 +68,7 @@ namespace MVVMTimer.ViewModels
             get => dateTime;
             set
             {
+                /*
                 if(dateTime != value)
                 {
                     dateTime = value;
@@ -32,15 +77,41 @@ namespace MVVMTimer.ViewModels
                         PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
                     }
                 }
+                */
 
-                /*
-                appDateTime = value;
+
+                dateTime = value;
                 var args = new PropertyChangedEventArgs(nameof(DateTime)); //name of the method here.
 
                 PropertyChanged?.Invoke(this, args);        //? is sugar to check if its null
-                */
+                
             }
 
+        }
+
+        
+        public string Timer
+        {
+           // get => timer;
+           get { return sCounter; }
+            set
+            {
+                timer = value;
+                var args = new PropertyChangedEventArgs(nameof(Timer));
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        public string stopWatchSeconds;
+        public string StopWatchSeconds
+        {
+            get { return stopWatchSeconds;  }
+            set
+            {
+                stopWatchSeconds = value;
+                var args = new PropertyChangedEventArgs(nameof(StopWatchSeconds));
+                PropertyChanged?.Invoke(this, args);
+            }
         }
     }
 }
